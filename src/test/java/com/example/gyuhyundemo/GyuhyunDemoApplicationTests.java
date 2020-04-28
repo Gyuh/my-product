9b81bb7286617e055761d4e40ff00b63685c5ac5
SKCC <SKCC@SKCC18D00081.SKCC.NET>
2020-04-28 10:25:47 +0900

    first product commit

---
+++ src/test/java/com/example/gyuhyundemo/GyuhyunDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gyuhyundemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyuhyunDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
